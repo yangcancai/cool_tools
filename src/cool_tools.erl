65b62e0de3bb3d3d6622f823949f0079bb9653b9
admin <admin.com>
2021-10-13 18:13:36 +0800

    add sha

--- src/cool_tools.erl
+++ src/cool_tools.erl
@@ -15,7 +15,7 @@
 %%% See the License for the specific language governing permissions and
 %%% limitations under the License.
 %%%
-   
+
 %%% @doc
 %%%
 %%% @end
@@ -26,4 +26,37 @@
 
 -author("yangcancai").
 
--export([]).
+-export([sha1/1, md5/1, to_upper/1, encode_login_password/2]).
+
+sha1(V) when is_integer(V) ->
+    sha1(erlang:integer_to_binary(V));
+sha1(V) when is_float(V) ->
+    sha1(erlang:float_to_binary(V));
+sha1(V) when is_atom(V) ->
+    sha1(erlang:atom_to_binary(V, utf8));
+sha1(V) ->
+    bin2hex(crypto:hash(sha, V)).
+
+md5(V) ->
+    bin2hex(crypto:hash(md5, V)).
+
+bin2hex(B) ->
+    L = binary_to_list(B),
+    LH0 = lists:map(fun(X) -> integer_to_list(X, 16) end, L),
+    LH = lists:map(fun ([X, Y]) ->
+                           [X, Y];
+                       ([X]) ->
+                           [$0, X]
+                   end,
+                   LH0), % add zeros
+    lists:flatten(LH).
+
+to_upper(Str) when is_list(Str) ->
+    erlang:list_to_binary(
+        string:to_upper(Str));
+to_upper(Str) when is_binary(Str) ->
+    to_upper(erlang:binary_to_list(Str)).
+
+encode_login_password(RealPass, Time) when is_binary(RealPass) ->
+    P = to_upper(sha1(RealPass)),
+    to_upper(md5(<<P/binary, (erlang:integer_to_binary(Time))/binary>>)).
